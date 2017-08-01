c7cd0dd021471d437d23fafdeb73d7d555a1a0dd
zongchaoyang <1044170311@qq.com>
2017-08-01 17:52:41 +0800

    新增api服务 mysql+sequelize

--- src/components/OrderCleanVegeTable.vue
+++ src/components/OrderCleanVegeTable.vue
@@ -1,17 +1,31 @@
 <!--网上订菜-->
 <template>
     <div>
-      网上订菜
+      <Vtree v-on:changeOrg="testaa"></Vtree>
+      <div style="width: 100%;position: relative">
+        <img src="../assets/img/reservebanner.png" style="max-width: 100%"/>
+        <Vnotice></Vnotice>
+      </div>
+
     </div>
 </template>
 
 <script>
+    import Vtree from "./OrgTree.vue"
+    import Vnotice from "./Notice.vue"
     export default {
         data () {
             return {}
         },
-
-        methods: {}
+      components: {
+        Vtree,
+        Vnotice
+      },
+        methods: {
+          testaa(data){
+            alert(data)
+          }
+        }
 
     }
 </script>
